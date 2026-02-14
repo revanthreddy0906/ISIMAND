c650d9e5cbb5f99622b5e2418bf1c0449b7d7f37
WhiteDevil <revanthreddy0906@gmail.com>
2026-02-14 17:06:24 +0530

    feat: introduce EfficientNetB0 model builder and update gitignore to exclude .vscode directory

---
+++ ml/efficientnet_model.py
@@ -0,0 +1,26 @@
+import tendorflow as tf
+from tensorflow.keras import models
+
+def build_efficientnet_model(num_classes=3):
+
+    base_model = tf.keras.applications.EfficientNetB0(
+        include_top=False,
+        weights="imagenet",
+        input_shape=(224, 224, 3)
+    )
+
+    base_model.trainable = False
+
+    inputs = tf.keras.Input(shape=(224, 224, 3))
+    x = base_model(inputs, training=False)
+    x = layers.GlobalAveragePooling2D()(x)
+
+    x = layers.Dense(128, activation="relu")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Dropout(0.5)(x)
+
+    outputs = layers.Dense(num_classes, activation="softmax")(x)
+
+    model = models.Model(inputs, outputs)
+
+    return model, base_model
